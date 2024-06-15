5fc951be6ad0fbd3d21c55ced52438b555060b26
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-06-15 13:05:58 +1000

    read log level from config

--- log.py
+++ log.py
@@ -1,4 +1,30 @@
 import logging
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+log_level_str = config.get("general", "log_level")
+
+match log_level_str.lower():
+    case "debug":
+        log_level = logging.DEBUG
+    
+    case "info":
+        log_level = logging.INFO
+    
+    case "warning":
+        log_level = logging.WARNING
+    
+    case "error":
+        log_level = logging.ERROR
+    
+    case "critical":
+        log_level = logging.CRITICAL
+    
+    case _:
+        log_level = logging.WARNING
+
 
 def setup_custom_logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
@@ -7,6 +33,6 @@ def setup_custom_logger(name):
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(log_level)
     logger.addHandler(handler)
     return logger
